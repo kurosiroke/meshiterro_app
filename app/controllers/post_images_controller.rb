e25a41f66c7995bc7c8b4ae2d6d8904bc9d30b2a
kurosiroke <ec2-user@ip-172-31-34-239.ap-northeast-1.compute.internal>
2023-04-19 13:36:30 +0000

    update

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,16 +16,13 @@ class PostImagesController < ApplicationController
   end
 
   def show
-  end
-
-  def show
     @post_image = PostImage.find(params[:id])
   end
 
   def destroy
     post_image = PostImage.find(params[:id])
-    post_image.削除
-    redirect_to PostImages_post
+    post_image.destroy
+    redirect_to post_image_path
   end
 
   private
