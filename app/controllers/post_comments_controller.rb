b01ba096d23dab7d1fc01c9e33adcf8ca20221bd
kurosiroke <ec2-user@ip-172-31-34-239.ap-northeast-1.compute.internal>
2023-04-21 00:31:08 +0000

    update

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,4 +1,4 @@
-class PostCommentController < ApplicationController
+class PostCommentsController < ApplicationController
 
   def create
     post_image = PostImage.find(params[:post_image_id])
@@ -6,6 +6,11 @@ class PostCommentController < ApplicationController
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
+  end
+  
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
